e1b79900221a0912cd13a91bed15b03d084f9602
Hiroyasu OHYAMA <hiroyasu-ohyama@Hiroyasu-no-MacBook-Pro.local>
2015-11-07 21:28:43 +0900

    add an implementation of echo server using generic gen_tcp module

---
+++ generic/server.erl
@@ -0,0 +1,39 @@
+-module(server).
+-export([start/1, init/1, recv/1]).
+
+start(Port) ->
+  gen_server:start_link({local, ?MODULE}, ?MODULE, [Port], []).
+
+init([Port]) ->
+  case gen_tcp:listen(Port, []) of
+  {ok, Socket} ->
+    wait_connect(Socket, 0)
+  end.
+
+wait_connect(ListenSocket, Count) ->
+  {ok, Socket} = gen_tcp:accept(ListenSocket),
+
+  % delegate Socket control to new process
+  gen_tcp:controlling_process(Socket,
+                              spawn(?MODULE, recv, [Socket])),
+
+  wait_connect(ListenSocket, Count + 1).
+
+recv(Socket) ->
+  receive
+    {tcp, Socket, Data} ->
+      io:format("receive data: ~p~n", [Data]),
+      case string:substr(Data, 1, 4) of
+        "quit" ->
+          close_socket(Socket);
+        _ ->
+          gen_tcp:send(Socket, "(Response) >> " ++ Data),
+          recv(Socket)
+      end;
+    {tcp_closed, Socket} ->
+      close_socket(Socket)
+  end.
+
+close_socket(Socket) ->
+  io:format("socket close~n"),
+  gen_tcp:close(Socket).
